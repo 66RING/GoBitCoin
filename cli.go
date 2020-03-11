bc69fb64739a3155b7c1442c42abfcc5343dc425
66RING <1552087953@qq.com>
2020-03-11 20:59:21 +0800

    simple transaction done

--- cli.go
+++ cli.go
@@ -12,11 +12,31 @@ type CLI struct {
 	blockchain *BlockChain
 }
 
+func (cli *CLI) createBlockChain(address string) {
+	bc := createBlockChain(address)
+	bc.db.Close()
+	fmt.Println("Create Success")
+}
+
+func (cli *CLI) getBalance(address string) {
+	bc := NewBlockChain(address)
+	defer bc.db.Close()
+	balance := 0
+	UTXOs := bc.FindUTXO(address) // Find balace
+	for _, out := range UTXOs {
+		balance += out.Value
+	}
+	fmt.Printf("Balance %s: %d \n", address, balance)
+}
+
 // print usage
 func (cli *CLI) printUsage() {
 	fmt.Println("Usage:")
 	fmt.Println("addblock: add block to blockchain")
 	fmt.Println("showchain: show blockchain")
+	fmt.Println("getbalance: get balance with address")
+	fmt.Println("createblockchain: create BlockChain with address")
+	fmt.Println("send -from From -to To -amount Amount: New transaction")
 }
 
 func (cli *CLI) validateArgs() {
@@ -26,42 +46,64 @@ func (cli *CLI) validateArgs() {
 	}
 }
 
-func (cli *CLI) addBlock(data string) {
-	//cli.blockchain.AddBlock(data)
-	fmt.Println("add block success")
-}
-
 func (cli *CLI) showBlockChian() {
-	bci := cli.blockchain.Iterator() // create iterator
-	for {
-		block := bci.next() // get next block
-		fmt.Printf("Previous hash: %x\n", block.PrevBlockHash)
-		//fmt.Printf("Transaction: %s\n", block.Transactions)
-		fmt.Printf("Current hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("Pow: %s", strconv.FormatBool(pow.Validate()))
-		fmt.Printf("\n\n")
+	bc := NewBlockChain("")
+	defer bc.db.Close()
 
-		if len(block.PrevBlockHash) == 0 { // if is the first block
+	bci := bc.Iterator()
+	for {
+		block := bci.next()
+		fmt.Printf("PrevBlockHash: %x", block.PrevBlockHash)
+		fmt.Printf("Current Hash: %x", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("pow %s \n", strconv.FormatBool(pow.Validate()))
+
+		if len(block.PrevBlockHash) == 0 {
 			break
 		}
 	}
 }
 
+func (cli *CLI) send(from, to string, amount int) {
+	bc := NewBlockChain(from)
+	defer bc.db.Close()
+	tx := NewUTXOTransaction(from, to, amount, bc)
+	bc.MineBLock([]*Transaction{tx})
+	fmt.Println("Transaction Success!")
+}
+
 func (cli *CLI) Run() {
 	cli.validateArgs()
-	addblockcmd := flag.NewFlagSet("addblock", flag.ExitOnError)
-	showchain := flag.NewFlagSet("showchain", flag.ExitOnError)
 
-	addBlockData := addblockcmd.String("data", "", "Block data")
+	showchaincmd := flag.NewFlagSet("showchain", flag.ExitOnError)
+	sendcmd := flag.NewFlagSet("send", flag.ExitOnError)
+	getbalancecmd := flag.NewFlagSet("getbalance", flag.ExitOnError)
+	createblockchaincmd := flag.NewFlagSet("createblockchaincmd", flag.ExitOnError)
+
+	getbalanceaddress := getbalancecmd.String("address", "", "get balance addree")
+	createblockaddress := createblockchaincmd.String("address", "", "get block addree")
+	sendfrom := sendcmd.String("from", "", "from who")
+	sendto := sendcmd.String("to", "", "to who")
+	sendamount := sendcmd.Int("amount", 0, "amount")
+
 	switch os.Args[1] {
-	case "addblock":
-		err := addblockcmd.Parse(os.Args[2:])
+	case "getbalance":
+		err := getbalancecmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "createblockchain":
+		err := createblockchaincmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "send":
+		err := sendcmd.Parse(os.Args[2:])
 		if err != nil {
 			log.Panic(err)
 		}
 	case "showchain":
-		err := showchain.Parse(os.Args[2:])
+		err := showchaincmd.Parse(os.Args[2:])
 		if err != nil {
 			log.Panic(err)
 		}
@@ -69,17 +111,31 @@ func (cli *CLI) Run() {
 		cli.printUsage()
 		os.Exit(1)
 	}
-	if addblockcmd.Parsed() {
-		if *addBlockData == "" {
-			addblockcmd.Usage()
+	if getbalancecmd.Parsed() {
+		if *getbalanceaddress == "" {
+			getbalancecmd.Usage()
 			os.Exit(1)
-		} else {
-			cli.addBlock(*addBlockData)
 		}
+		cli.getBalance(*getbalanceaddress)
 	}
 
-	if showchain.Parsed() {
+	if createblockchaincmd.Parsed() {
+		if *createblockaddress == "" {
+			createblockchaincmd.Usage()
+			os.Exit(1)
+		}
+		cli.createBlockChain(*createblockaddress)
+	}
+
+	if sendcmd.Parsed() {
+		if *sendfrom == "" || *sendto == "" || *sendamount <= 0 {
+			sendcmd.Usage()
+			os.Exit(1)
+		}
+		cli.send(*sendfrom, *sendto, *sendamount)
+
+	}
+	if showchaincmd.Parsed() {
 		cli.showBlockChian()
 	}
-
 }
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 func main() {
-	//block := NewBlockChain()
-	//defer block.db.Close()
-	//cli := CLI{block}
-	//cli.Run()
+	cli := CLI{}
+	cli.Run()
 }
